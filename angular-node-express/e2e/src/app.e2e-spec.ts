c14ea090aa268851e19c3ba99b805b8510f44f35
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-03-19 01:33:13 +0800

    Initial setup

--- angular-node-express/e2e/src/app.e2e-spec.ts
+++ angular-node-express/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('blog app is running!');
+    expect(page.getTitleText()).toEqual('angular-node-express app is running!');
   });
 
   afterEach(async () => {
--- angular-node-express/src/app/app.component.spec.ts
+++ angular-node-express/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'blog'`, () => {
+  it(`should have as title 'angular-node-express'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('blog');
+    expect(app.title).toEqual('angular-node-express');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('blog app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-node-express app is running!');
   });
 });
