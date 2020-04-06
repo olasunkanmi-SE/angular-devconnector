39a0f8ab7a2e0ea8778196c91f2dfb192c707a35
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-04-06 11:56:14 +0800

    create post model

---
+++ server/models/Post.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    body: {
+        type: String,
+        required: true
+    },
+
+    name: {
+        type: String
+    },
+
+    avatar: {
+        type: String
+    },
+
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+                required: true
+            },
+
+            body: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+const Post = mongoose.model('post'.postSchema);
+
+exports.Post = Post;
