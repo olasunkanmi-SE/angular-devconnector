75b8772a0f28418f890906797fabc24db78509f2
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-04-13 22:53:52 +0800

    implement UserController

---
+++ server/controllers/user.js
@@ -0,0 +1,80 @@
+const { User, validate } = require('../models/User');
+const gravatar = require('gravatar');
+const _ = require('lodash');
+const bcrypt = require('bcryptjs');
+
+
+exports.getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find().select('_id name email avatar date');
+        response = {
+            count: users.length,
+            users: users.map(user => {
+                return {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    avatar: user.avatar,
+                    date: user.date,
+                    request: {
+                        type: 'GET',
+                        url: `http://localhost:3000/api/users/${user._id}`
+                    }
+                }
+            })
+        }
+        users.length > 0 ? res.json(response) : res.status(404).send('No users found');
+    } catch (error) {
+        return res.status(400).send(error);
+
+    }
+
+
+}
+
+exports.createUser = async (req, res, next) => {
+    try {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+        try {
+            let user = await User.findOne({ email: req.body.email });
+            if (user) return res.status(400).json({ email: 'user already exists' });
+            const avatar = gravatar.url(req.body.email, { s: 200, r: 'pg', d: 'mm' });
+            user = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+                repeat_password: req.body.repeat_password,
+                gender: req.body.gender,
+                avatar,
+            });
+
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(user.password, salt);
+
+            await user.save();
+            return res.status(201).json({
+                message: "User created successfully",
+                createdUser: {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    avatar: user.avatar,
+                    date: user.date,
+                    request: {
+                        type: 'GET',
+                        url: `http://localhost:3000/api/users/${user._id}`
+                    }
+                }
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    } catch (error) {
+        return res.status(400).send(error);
+
+    }
+
+
+}
+
