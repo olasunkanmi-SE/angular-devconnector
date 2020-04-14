99c506b7e2fb13c77e804937e6b0b4acb29476db
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-04-14 20:21:29 +0800

    create profile Controller and refactor profile route

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,225 +1,50 @@
+const passport = require('passport');
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const { Profile } = require('../../models/Profile');
-const { User } = require('../../models/User');
-const err = require('../../../server/error');
-const validateProfile = require('../../validation/profile');
-const validateExperience = require('../../validation/experience');
-const validateEducation = require('../../validation/education');
+const profileController = require('../../controllers/profile');
+
 
 
 //Get Current Profile
 
-router.get('/current/user', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
-
-    try {
-        const profile = (await Profile.findOne({ user: req.user._id }));
-        await profile.populate('user', ['name', 'avatar']).execPopulate();
-        if (!profile) return res.status(404).json(err.profileError.noUserProfile);
-        return res.status(201).json(profile);
-    } catch (ex) {
-        next(ex);
-    }
-});
+router.get('/current/user', passport.authenticate('jwt', { session: false }), profileController.getCurrentUser);
 
 //Create and Update Profile
 
-router.post('/', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
-    try {
-        const { errors, isValid } = validateProfile(req.body);
-        if (!isValid) return res.status(400).json(errors);
-
-        const profileFields = {};
-        profileFields.user = req.user.id;
-        if (req.body.handle) profileFields.handle = req.body.handle;
-        if (req.body.company) profileFields.company = req.body.company;
-        if (req.body.website) profileFields.website = req.body.website;
-        if (req.body.location) profileFields.location = req.body.location;
-        if (req.body.bio) profileFields.bio = req.body.bio;
-        if (req.body.status) profileFields.status = req.body.status;
-        if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
-        // Skills Comma seperated values
-        if (typeof req.body.skills !== undefined) {
-            profileFields.skills = req.body.skills.split(',');
-        }
-        // Socials
-        profileFields.socials = {};
-        if (req.body.youtube) profileFields.socials.youtube = req.body.youtube;
-        if (req.body.twitter) profileFields.socials.twitter = req.body.twitter;
-        if (req.body.facebook) profileFields.socials.facebook = req.body.facebook;
-        if (req.body.linkedin) profileFields.socials.linkedin = req.body.linkedin;
-        if (req.body.instagram) profileFields.socials.instagram = req.body.instagram;
-
-        let profile = await Profile.findOne({ user: req.user.id });
-        if (profile) {
-            profile = await Profile.findByIdAndUpdate(
-                { _id: profile._id },
-                { $set: profileFields },
-                { new: true }
-            )
-            return res.json(profile);
-        } else {
-            let profile = await Profile.findOne({ handle: profileFields.handle });
-            if (profile) return res.status(400).json(err.profileError.handleExists);
-            profile = new Profile(profileFields);
-            await profile.save();
-            res.json(profile);
-
-        }
-
-    } catch (ex) {
-        next(ex);
-    }
-
-})
+router.post('/', passport.authenticate('jwt', { session: false }), profileController.createOrUpdateUser);
 
 //Get Profile by Handle
 
-router.get('/handle/:handle', async (req, res, next) => {
-    try {
-        let profile = await Profile.findOne({ handle: req.params.handle });
-        await profile.populate('user', ['name', 'avatar']).execPopulate();
-        if (!profile) return res.status.json(err.profileError.noProfile);
-        res.json(profile);
-    } catch (ex) {
-        next(ex);
-    }
-
-})
+router.get('/handle/:handle', profileController.getProfileByHandle);
 
 //Get Profile by Id
 
-router.get('/:id', async (req, res, next) => {
-    try {
-        let profile = await Profile.findById({ _id: req.params.id });
-        await profile.populate('user', ['name', 'avatar']).execPopulate();
-        if (!profile) return res.status.json(err.profileError.noProfile);
-        res.json(profile);
-    } catch (ex) {
-        next(ex);
-    }
-})
+router.get('/:id', profileController.getProfileById);
 
 //Get all profiles
 
+router.get('/', profileController.getProfiles);
 
-router.get('/', (req, res, next) => {
-    try {
-        Profile.find().populate('user', ['name', 'avatar'])
-            .then(profiles => {
-                if (!profiles) return res.status(404).json(err.profileError.noProfiles)
-                res.status(201).json(profiles);
-            })
+//Create a new professional experience
 
-    } catch (ex) {
-        next(ex);
-    }
-})
-
-//Create a new experience
-
-router.post('/experience', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
-    try {
-        const { errors, isValid } = validateExperience(req.body);
-        if (!isValid) return res.status(400).json(errors);
-
-        let profile = await Profile.findOne({ user: req.user.id });
-        const userExperience = {
-            title: req.body.title,
-            employmenttype: req.body.employmenttype,
-            company: req.body.company,
-            location: req.body.location,
-            current: req.body.current,
-            endlastposition: req.body.endlastposition,
-            startdate: req.body.startdate,
-            enddate: req.body.enddate,
-            updateheadline: req.body.updateheadline,
-            headline: req.body.headline,
-            description: req.body.description
-        }
-        //Unshift like push is used to add data to an array but to the beginning of the array
-        profile.experience.unshift(userExperience);
-        await profile.unshift({ message: 'profile created successfully' }).save();
-        return res.status(201).json(profile);
-
-    } catch (ex) {
-        next(ex);
-    }
-
-})
+router.post('/experience', passport.authenticate('jwt', { session: false }), profileController.createUserExperience);
 
 //Create new Education
 
-router.post('/education', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
-    try {
-        const { errors, isValid } = validateEducation(req.body);
-        if (!isValid) return res.status(400).json(errors);
-
-        const userEducation = {
-            school: req.body.school,
-            degree: req.body.degree,
-            fieldofstudy: req.body.fieldofstudy,
-            startyear: req.body.startyear,
-            endyear: req.body.endyear,
-            grade: req.body.grade,
-            description: req.body.description
-
-        }
-
-        let profile = await Profile.findOne({ user: req.user.id });
-        profile.education.unshift(userEducation);
-        await profile.save();
-        return res.status(201).json(profile);
-
-    } catch (ex) {
-        next(ex);
-    }
-})
-
+router.post('/education', passport.authenticate('jwt', { session: false }), profileController.createUserEducation);
 
 //Delete an experience by ID
 
-router.delete('/experience/:id', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
-    try {
-        let profile = await Profile.findOne({ user: req.user.id });
-        const removeProfile = profile.experience.map((items) => items.id).indexOf(req.params.id);
-        console.log(removeProfile)
-        profile.experience.splice(removeProfile, 1);
-        profile.save();
-        return res.status(201).json(profile.push());
-
-    } catch (ex) {
-        next(ex);
-    }
-})
+router.delete('/experience/:id', passport.authenticate('jwt', { session: false }), profileController.deleteUserExperienceById);
 
 // Delete an Education by ID
 
-router.delete('/education/:id', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
-    try {
-        let profile = await Profile.findOne({ user: req.user.id });
-        const removeEducation = profile.education.map(items => items.id).indexOf(req.params.id);
-        profile.education.splice(removeEducation, 1)
-        return res.status(201).json(profile);
-
-    } catch (ex) {
-        next(ex);
-    }
-})
+router.delete('/education/:id', passport.authenticate('jwt', { session: false }), profileController.deleteUserEducationById)
 
 //Delete a user and profile
 
-router.delete('/', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
-    try {
-        await Profile.findOneAndRemove({ user: req.user.id });
-        await User.findOneAndRemove({ _id: req.user.id });
-        return res.status(201).json({ success: true });
+router.delete('/', passport.authenticate('jwt', { session: false }), profileController.deleteUserAndProfile)
 
-    } catch (ex) {
-        next(ex);
-    }
-})
 
 // router.put('/education', passport.authenticate('jwt', { session: false }), async (req, res) => {
 //     try {
