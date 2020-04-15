ff0595cc73f99445512bbe9204415fa202630871
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-04-15 15:21:14 +0800

    add navbar and navmenu

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const post = require('./server/routes/api/post');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const app = express();
-const fs = require('fs');
 
 
 
---
+++ src/app/core/components/main-menu/main-menu.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-main-menu',
+  templateUrl: './main-menu.component.html',
+  styleUrls: ['./main-menu.component.css']
+})
+export class MainMenuComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
