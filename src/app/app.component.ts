b7650baac702b0364a71f8bacaa9e5cd0b5a68a3
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-04-15 20:07:42 +0800

    implement the signup page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  hide: boolean = true;
+
   constructor() {}
 
   ngOnInit() {}
--- src/app/core/modules/md-components/md-components.module.ts
+++ src/app/core/modules/md-components/md-components.module.ts
@@ -2,9 +2,24 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatRadioModule } from "@angular/material/radio";
 import { NgModule } from "@angular/core";
 
 @NgModule({
-  exports: [MatMenuModule, MatIconModule, MatButtonModule, MatToolbarModule],
+  exports: [
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatRadioModule,
+  ],
 })
 export class MdComponentsModule {}
