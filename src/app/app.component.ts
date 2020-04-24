5b210d4f627f22962820413a994df813650584a0
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-04-24 11:18:29 +0800

    implement storage services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { StorageService } from "./core/storage/storage.service";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
@@ -6,7 +7,23 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  constructor() {}
+  govParams: {};
+  x: {};
+  y: any;
+  constructor(private storage: StorageService) {
+    this.govParams = {
+      params: {
+        statusCode: 0,
+        paid: false,
+      },
+    };
+    const mystate = 5;
+
+    this.storage.setLocalObject("govparams", this.govParams);
+    this.x = this.storage.getLocalObject("govparams");
+    this.y = this.storage.saveItem("state", mystate);
+    console.log(this.x);
+  }
 
   ngOnInit() {}
 }
--- src/app/core/storage/storage.service.ts
+++ src/app/core/storage/storage.service.ts
@@ -1,9 +1,47 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Key } from "protractor";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class StorageService {
+  govParams: {};
+  constructor() {}
 
-  constructor() { }
+  saveToken(token: string): void {
+    localStorage.setItem("jwt_token", token);
+  }
+
+  getToken() {
+    return localStorage.getItem("jwt_token");
+  }
+
+  removeToken(): void {
+    localStorage.removeItem("$token");
+  }
+
+  saveItem(alias: string, item: any): void {
+    localStorage.setItem(alias, item);
+  }
+
+  getItem(alias: string) {
+    return localStorage.getItem(alias);
+  }
+
+  removeItem(alias: string): void {
+    localStorage.removeItem(alias);
+  }
+
+  setLocalObject(key: string, value: any): void {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  getLocalObject(key: string) {
+    const localObject = localStorage.getItem(key);
+    return JSON.parse(localObject);
+  }
+
+  removeLocalObject(key: string): void {
+    return localStorage.removeItem(key);
+  }
 }
