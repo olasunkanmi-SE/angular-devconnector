651b28855c147999a54c1b3bf59e3b267f7b3d5c
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-04-24 09:58:02 +0800

    define error codes

--- src/app/core/auth/services/http/http.service.ts
+++ src/app/core/auth/services/http/http.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { ApiMethod, AuthEndPoints } from "./consts";
+import { ErrorService } from "./../error/error.service";
+import { environment } from "./../../../../../environments/environment.prod";
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class HttpService {
-
-  constructor() { }
+  constructor() {}
 }
