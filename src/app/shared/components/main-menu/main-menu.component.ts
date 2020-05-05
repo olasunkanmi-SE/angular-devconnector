829add7ff258765045018f6b2eb080c400c7bce0
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-05-05 13:40:39 +0800

    display menu buttons based on user Auth state

--- src/app/shared/components/main-menu/main-menu.component.ts
+++ src/app/shared/components/main-menu/main-menu.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { AuthService } from "./../../../core/auth/services/auth/auth.service";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 import { map, shareReplay } from "rxjs/operators";
 import { FormControl } from "@angular/forms";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
@@ -10,7 +11,7 @@ import { faHome } from "@fortawesome/free-solid-svg-icons";
   templateUrl: "./main-menu.component.html",
   styleUrls: ["./main-menu.component.css"],
 })
-export class MainMenuComponent {
+export class MainMenuComponent implements OnInit, OnDestroy {
   faUser = faHome;
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
@@ -21,6 +22,23 @@ export class MainMenuComponent {
 
   menuControl = new FormControl();
   options: string[] = ["One", "Two", "Three"];
+  private authListenerSubscription: Subscription;
+  userAuthenticated: boolean = false;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.authListenerSubscription = this.authService
+      .getAuthStatusListener()
+      .subscribe((isAuthenticated) => {
+        this.userAuthenticated = isAuthenticated;
+      });
+  }
+
+  ngOnDestroy() {
+    this.authListenerSubscription.unsubscribe();
+  }
 }
