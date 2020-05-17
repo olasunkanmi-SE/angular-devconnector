5853d269a3f88ef0c632d9809fd1f92d18450f26
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-05-17 15:16:59 +0800

    minor fix on the post validator

--- server/validation/post.js
+++ server/validation/post.js
@@ -3,13 +3,13 @@ const isEmpty = require('./is-empty');
 
 module.exports = function validateUserPost(data) {
     let errors = {};
-    data.title = !isEmpty(data.text) ? data.text : '';
+    data.text = !isEmpty(data.text) ? data.text : '';
 
     if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
         errors.text = 'Post must be between 10 and 300 characters'
     }
 
-    if (Validator.isEmpty(data.text)) errors.text = 'please enter your text';
+    if (Validator.isEmpty(data.text)) errors.text = 'please enter texts to create post';
 
     return {
         errors,
