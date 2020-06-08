526b39bb065e1025c5ca2442a3b9369a07d12959
raymond-frontend <raymond.oyinlola@viewqwest.com>
2020-06-08 17:27:19 +0800

    create car brand controllers and routes

---
+++ server/controllers/carbrand.js
@@ -0,0 +1,39 @@
+const { Brand, validate } = require('../models/CarBrand');
+
+
+module.exports.create = async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    try {
+        brand = new Brand({
+            name: req.body.name
+        })
+        await brand.save();
+        return res.status(201).json({
+            brand: {
+                id: brand._id,
+                name: brand.name
+            }
+        })
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+module.exports.brands = async (req, res) => {
+    const brands = await Brand.find();
+    if (brands) {
+        return res.status(200).json(brands);
+    }
+    return res.status(404).json('No car brands available at the moment');
+
+}
+
+module.exports.brand = async (req, res) => {
+    const brand = await Brand.findById(req.params.id);
+    if (brand) {
+        return res.status(200).json(brand);
+    }
+    return res.status(404).json('car brand not found');
+}
--- server/routes/api/carbrand.js
+++ server/routes/api/carbrand.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middlewares/auth');
+const brandController = require('../../controllers/carbrand');
+
+
+router.post('/create', auth, brandController.create);
+
+router.get('/', auth, brandController.brands);
+
+router.get('/brand', auth, brandController.brand);
